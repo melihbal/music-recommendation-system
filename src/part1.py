c789761a5dc1c8e964e4fea6225c6076c7e90567
Gülce Tahtasız <gulcetahtasiz@Gulces-MacBook-Air.local>
2025-12-14 20:33:21 +0300

    Complete Part 1 and Part 2

--- src/part1.py
+++ src/part1.py
@@ -9,13 +9,167 @@ Estimate how likely a song is to receive a 5★ rating using conditional probabi
 import pandas as pd
 import numpy as np
 
+
+
+def load_global_data():
+    tracks = pd.read_csv("../data/tracks.csv")
+    ratings = pd.read_csv("../data/ratings.csv")
+
+    # merge the tracks and ratings file based on tracj_id = song_id, to see everything on the same file
+    df = ratings.merge(tracks, left_on="song_id", right_on="track_id")
+
+    #add extra column to see if the song is rated as 5 star
+    df["is_5star"] = (df["rating"] == 5).astype(int)
+
+    return df
+
+
+def load_gulces_personal_data():
+    tracks = pd.read_csv("../data/tracks.csv")
+    gulce_ratings = pd.read_csv("../data/gulce_ratings.csv")
+
+    df = gulce_ratings.merge(tracks, left_on="song_id", right_on="track_id")
+    df["is_5star"] = (df["rating"] == 5).astype(int)
+    return df
+
+
+def load_melihs_personal_data():
+    tracks = pd.read_csv("../data/tracks.csv")
+    melih_ratings = pd.read_csv("../data/melih_ratings.csv")
+
+    df = melih_ratings.merge(tracks, left_on="song_id", right_on="track_id")
+    df["is_5star"] = (df["rating"] == 5).astype(int)
+    return df
+
+
+
+def p_5_given(df, feature, value, alpha=1):
+    """
+    Computes P(5★ | feature = value) with Laplace smoothing
+    """
+
+    #this subset just takes the rows with given feature
+    subset = df[df[feature] == value]
+
+    #total ratings with given feature
+    total = len(subset)
+    if total == 0:
+        return None
+    
+    #number of 5 star ratings & given features count
+    num_5 = subset["is_5star"].sum()
+    
+    # conditional probability = (5 star & feature) / feature with laplace
+    return (num_5 + alpha) / (total + 2 * alpha)
+
+
+def p_5_given_two(df, feature1, value1, feature2, value2, alpha=1):
+    """
+    P(5★ | feature1=value1, feature2=value2) with Laplace smoothing
+    """
+    # subset for feature combinations
+    subset = df[
+        (df[feature1] == value1) &
+        (df[feature2] == value2)
+    ]
+
+    total = len(subset)
+    if total == 0:
+        return None
+
+    num_5 = subset["is_5star"].sum()
+    return (num_5 + alpha) / (total + 2 * alpha)
+
+
+def bayes(df, feature, value):
+    """
+    Computes P(feature=value | 5★) using Bayes' rule
+    """
+    # P(5★ | feature)
+    p_5_given_f = p_5_given(df, feature, value)
+    if p_5_given_f is None:
+        return None
+
+    # P(feature)
+    p_f = len(df[df[feature] == value]) / len(df)
+
+    # P(5★)
+    p_5 = df["is_5star"].mean()
+
+    return (p_5_given_f * p_f) / p_5
+
+
+
+
 def main():
-    print("Part 1: Conditional Probability Modeling")
-    print("TODO: Implement conditional probability calculations")
-    print("- Load tracks.csv and ratings.csv")
-    print("- Compute P(5★ | Artist), P(5★ | Year), P(5★ | Popularity)")
-    print("- Apply Laplace smoothing")
-    print("- Use Bayes' rule to invert relationships")
+
+    #Load tracks.csv and ratings.csv
+    global_df = load_global_data()
+    gulce_df= load_gulces_personal_data()
+    #melih_df= load_melihs_personal_data()
+
+    #Compute P(5★ | Artist), P(5★ | Year), P(5★ | Popularity)
+    # some conditional probabilities with one feature
+    print("P(5★ | Artist = Doja Cat):",
+          p_5_given(global_df, "primary_artist_name", "Doja Cat"))
+
+    print("P(5★ | Year = 2014):",
+          p_5_given(global_df, "album_release_year", 2016))
+    
+    print("P(5★ | danceability: danceable):",
+          p_5_given(global_df, "ab_danceability_value", "danceable"))
+    
+    print("P(5★ | genre = pop):",
+          p_5_given(global_df, "ab_genre_rosamerica_value", "pop"))
+
+    print("P(5★ | Explicit = True):",
+          p_5_given(global_df, "explicit", True))
+    
+
+    # some conditional probabilities with two features
+    print("P(5★ | Artist = The Chainsmokers & release year = 2016):",
+          p_5_given_two(global_df,"primary_artist_name","The Chainsmokers","album_release_year", 2016))
+
+    print("P(5★ | danceability: danceable & genre = pop):",
+          p_5_given_two(global_df,"ab_danceability_value","danceable","ab_genre_rosamerica_value", "pop"))
+    
+    #bayes rule implementation
+    print("P(Artist = Halsey | 5★):",
+      bayes(global_df, "primary_artist_name", "Halsey"))
+
+    print("P(Artist = Taylor Swift | 5★):",
+      bayes(global_df, "primary_artist_name", "Taylor Swift"))
+    
+
+
+
+    # GULCE'S PROB
+    print("P(5★ | Artist = Doja Cat):",
+          p_5_given(gulce_df, "primary_artist_name", "Doja Cat"))
+    
+    print("P(5★ | genre = pop):",
+          p_5_given(gulce_df, "ab_genre_rosamerica_value", "pop"))
+
+    print("P(5★ | danceability: danceable):",
+          p_5_given(gulce_df, "ab_danceability_value", "danceable"))
+    
+
+
+    # MELIH'S PROB
+    #print("P(5★ | Artist = Doja Cat):",
+    #      p_5_given(melih_df, "primary_artist_name", "Doja Cat"))
+    
+    #print("P(5★ | genre = pop):",
+    #      p_5_given(melih_df, "ab_genre_rosamerica_value", "pop"))
+
+    #print("P(5★ | danceability: danceable):",
+    #      p_5_given(melih_df, "ab_danceability_value", "danceable"))
+
+
+    # GROUP PROB = total prob / 2 for the same feature
+    # group prob = (p_5_given(melih_df, "primary_artist_name", "Doja Cat") + p_5_given(gulce_df, "primary_artist_name", "Doja Cat")) / 2
+
+
 
 if __name__ == "__main__":
     main()
--- src/part2.py
+++ src/part2.py
@@ -10,14 +10,138 @@ geometric and Beta-geometric distributions.
 import pandas as pd
 import numpy as np
 from scipy import stats
+from scipy.special import beta as beta_func
+
+
+# compute how many ratings until 5* one and put them in an array
+def compute_Tu(ratings):
+    """
+    Compute T_u for each user: 
+    the round index of the first 5★ rating
+    """
+    Tu = []
+
+    for user, df_u in ratings.groupby("user_id"):
+        five_star_rounds = df_u[df_u["rating"] == 5]["round_idx"]
+
+        if len(five_star_rounds) > 0:
+            Tu.append(five_star_rounds.min())
+
+    return np.array(Tu)
+
+# estimation of general p for geometric distribution
+def fit_geometric(Tu):
+    """
+    Estimate p for geometric distribution
+    """
+    mean_T = np.mean(Tu)
+    p_hat = 1 / mean_T
+    return p_hat
+
+# geometric distribution
+def geometric_pmf(t, p):
+    return (1 - p)**(t - 1) * p
+
+# beta-geometric distribution
+def beta_geometric_pmf(t, alpha, beta):
+    return beta_func(alpha + 1, beta + t - 1) / beta_func(alpha, beta)
+
+
+
 
 def main():
-    print("Part 2: User Variability Modeling")
-    print("TODO: Implement user variability analysis")
+
+    ratings = pd.read_csv("../data/ratings.csv")
+
+
+    Tu = compute_Tu(ratings)
+    p_hat = fit_geometric(Tu)
+
+
     print("- Calculate time-to-5★ for each user")
+    print(Tu)
+
+
+    # empirical probability = T = n, (how many users found 5* at nth count) / total user count
+    unique, counts = np.unique(Tu, return_counts=True)
+    empirical_pmf = counts / counts.sum()
+
+    print("Empirical distribution:")
+    for t, p in zip(unique, empirical_pmf):
+        print(f"T = {t}: {p:.3f}")
+
+
     print("- Fit geometric distribution")
+    print("Geometric model prediction:")
+    for t in unique:
+        print(f"T = {t}: {geometric_pmf(t, p_hat):.3f}")
+
+
+    # paramaters for a and b
+    params = [
+    (1, 5),   # pickier users
+    (3, 5),   # a bit picky
+    (2, 2),   # balanced
+    (5, 1)    # users finding a favourite quickly
+    ]
+
+
     print("- Fit Beta-geometric distribution")
+    print("Beta-Geometric model prediction:")
+    for alpha, beta in params:
+        print(f"\nBeta-Geometric (α={alpha}, β={beta}):")
+        for t in unique:
+            print(f"T={t}: {beta_geometric_pmf(t, alpha, beta):.3f}")
+
+
+
     print("- Perform hypothesis testing between user groups")
+    #mergeing the tracks with ratings to correctly group them
+    ratings = pd.read_csv("../data/ratings.csv")
+    tracks = pd.read_csv("../data/tracks.csv")
+    df = ratings.merge(tracks, left_on="song_id", right_on="track_id")
+
+    first_fives = (
+    df[df["rating"] == 5]
+    .sort_values("round_idx")
+    .groupby("user_id")
+    .first()
+    .reset_index()
+    )
+
+    # hypothesis test due to album release year
+    group_old = first_fives[first_fives["album_release_year"] < 2000]
+    group_new = first_fives[first_fives["album_release_year"] >= 2000]
+
+    Tu_old = group_old["round_idx"].values
+    Tu_new = group_new["round_idx"].values
+
+    print("users who likes songs released before year 2000", Tu_old)
+    print("users who likes songs released after year 2000",Tu_new)
+
+    stat, p_value1 = stats.mannwhitneyu(Tu_old, Tu_new, alternative="two-sided")
+    print("p-value by release year:", p_value1)
+
+    print("Mean T (old):", Tu_old.mean())
+    print("Mean T (new):", Tu_new.mean())
+
+    # hypothesis test due to popularity
+    group_notpopular= first_fives[first_fives["track_popularity"] < 80]
+    group_popular = first_fives[first_fives["track_popularity"] >= 80]
+
+    Tu_notpopular = group_notpopular["round_idx"].values
+    Tu_popular= group_popular["round_idx"].values
+
+    print("users who likes songs that are not popular", Tu_notpopular)
+    print("users who likes songs that are popular",Tu_popular)
+
+    stat, p_value2 = stats.mannwhitneyu(Tu_notpopular, Tu_popular, alternative="two-sided")
+    print("p-value by popularity:", p_value2)
+
+    print("Mean T not popular:", Tu_notpopular.mean())
+    print("Mean T popular:", Tu_popular.mean())
+
+
 
 if __name__ == "__main__":
     main()
